d2ca1923a3ca495b6826ca20d2b9969862f525b1
Andy Goldschmidt <andy@Andys-MacBook-Air.local>
2019-12-03 13:41:32 -0800

    Added classmethods for full data matrix constructors

--- DMD.py
+++ DMD.py
@@ -6,10 +6,7 @@
 # - Should we create an ABC interface for DMD?
 # - Should we store the DMD result as a scipy state space model (dlsim)?
 # - Else, should simulate have options 'continuous' and 'euler'? (at interface)
-# - Predict from arbitrary initial conditions
-# - Better zero control behavior (string 'Zero'?)
 #-----
-import abc
 import numpy as np
 from numpy.linalg import svd, pinv, eig
 import matplotlib.pyplot as plt
@@ -17,8 +14,6 @@ import matplotlib.pyplot as plt
 # ------------------------------------------------------
 # -- Helper methods
 # ------------------------------------------------------
-color = plt.rcParams['axes.prop_cycle'].by_key()['color'] # store color array
-
 def delay_embed(X, shift):
     '''
     Delay-embed the matrix X with measurements from future times.
@@ -56,37 +51,34 @@ def plot_eigs(eigs, **kwargs):
 # -- Main class
 # ------------------------------------------------------
 class DMD:
-    def __init__(self, **kwargs):
+    def __init__(self, X2, X1, ts, **kwargs):
         '''
         Parameters:
-            X (required):
-                Data matrix with columns storing states at sequential time measurements
-                optional alternative: explicitely set X1 and X2
-            sample_times (required):
-                Sequential time measurements matching X (or X1).
-            dmd_modes: 'exact'
-            threshold: None
+            X2, X1:
+                Data matrices with columns storing states at sequential time measurements,
+                such that X2 = A X1.
+            ts:
+                Sequential time series at which the X (or X1) measurements were taken.
+
+        Optional parameters:
+            dmd_modes: default 'exact'
+            threshold: default None
 
         Updates:
+            self.X2,self.X1: data
             self.t0: initial time
             self.dt: timestep
             self.orig_timesteps: list of times for X1
-            self.A, self.Atilde: dynamical operator
+            self.A: discrete time dynamical operator
             self.eigs: eigenvalues of Atilde
             self.modes: eigenvectors of A and Atilde
         '''
-        X = kwargs.get('X')
-        if X is not None:
-            self.X1 = X[:, :-1]
-            self.X2 = X[:, 1:]
-        else:
-            self.X1 = kwargs['X1']
-            self.X2 = kwargs['X2']
+        self.X2 = X2
+        self.X1 = X1
 
-        sample_times = kwargs['sample_times']
-        self.t0 = sample_times[0]
-        self.dt = sample_times[1] - sample_times[0]
-        self.orig_timesteps = sample_times[:-1]
+        self.t0 = ts[0]
+        self.dt = ts[1] - ts[0]
+        self.orig_timesteps = ts if len(ts) == len(self.X1) else ts[:-1]
 
         dmd_modes = kwargs.get('dmd_modes', 'exact')
         threshold = kwargs.get('threshold')
@@ -98,11 +90,11 @@ class DMD:
             U = U[:,:r]
             S = S[:r]
             Vt = Vt[:r,:]
-        self.Atilde = dag(U)@self.X2@dag(Vt)@np.diag(1/S)
+        Atilde = dag(U)@self.X2@dag(Vt)@np.diag(1/S)
         self.A = self.X2@dag(Vt)@np.diag(1/S)@dag(U)
 
         # II. Atilde W = W Y (Eigendecomposition)
-        self.eigs, W = eig(self.Atilde)
+        self.eigs, W = eig(Atilde)
 
         # III. Two versions (eigenvectors of A)
         #      DMD_exact = X2 V S^-1 W 
@@ -112,7 +104,16 @@ class DMD:
         elif dmd_modes == 'projected':
             self.modes = U@W
         else:
-            raise ValueError('In DMD initialization, unknown dmd_mode type.')
+            raise ValueError('In DMD initialization, unknown dmd_mode type.')        
+
+    @classmethod
+    def from_full(cls, X, ts, **kwargs):
+        '''
+        Construct the LHS and RHS of the DMD model using the full data matrix.
+        '''
+        X1 = X[:, :-1]
+        X2 = X[:, 1:]
+        return cls(X2, X1, ts, **kwargs)
 
     def time_spectrum(self, t):
         '''
@@ -128,15 +129,16 @@ class DMD:
         else:
             return np.array([self.time_spectrum(it) for it in t]).T
         
-    def predict(self, t=None):
+    def predict(self, t=None, x0=None):
         ''' 
         Return the predicted future state according to the continous approximation to A.
 
         Default is to predict along the original timesteps.
         '''
-        t = self.orig_timesteps if t is None else t 
+        x0 = self.X1[:,0] if x0 is None else x0
+        t = self.orig_timesteps if t is None else t
         left = self.modes
-        right = pinv(self.modes)@self.X1[:,0]
+        right = pinv(self.modes)@x0
         if np.isscalar(t):
             return left@np.diag(self.time_spectrum(t))@right
         else:
@@ -146,19 +148,45 @@ class DMD:
 # -- DMD with control (DMDc)
 # ------------------------------------------------------
 class DMDc:   
-    def __init__(self, X, Ups, sample_times, threshold=None):      
-        self.X1 = X[:, :-1]
-        self.X2 = X[:, 1:]
-        self.Ups = Ups[:,:-1] if Ups.shape[1]==len(sample_times) else Ups # ONLY these 2 options
+    def __init__(self, X2, X1, Ups, ts, **kwargs):
+        '''
+        Parameters:
+            X2, X1:
+                Data matrices with columns storing states at sequential time measurements,
+                such that X2 = A X1 + B U
+            Ups:
+                Control matrix with columns storing control signals at sequential times.
+            ts:
+                Sequential time series at which the X (or X1) measurements were taken.
+
+        Optional parameters:
+            threshold: default None
+
+        Updates:
+            self.X2,self.X1: data
+            self.Ups: control
+            self.t0: initial time
+            self.dt: timestep
+            self.orig_timesteps: list of times for X1
+            self.A (self.Atilde): discrete time dynamical operator (projected)
+            self.B (self.Btilde): discrete time control operator (projected)
+            self.eigs: eigenvalues of Atilde
+            self.modes: eigenvectors of A and Atilde
+        '''
+        self.X1 = X1
+        self.X2 = X2
+        self.Ups = Ups[:,:-1] if Ups.shape[1]==len(ts) else Ups # ONLY these 2 options
         
-        self.t0 = sample_times[0]
-        self.dt = sample_times[1] - sample_times[0]
-        self.orig_timesteps = sample_times[:-1]
+        self.t0 = ts[0]
+        self.dt = ts[1] - ts[0]
+        self.orig_timesteps = ts if len(ts) == len(self.X1) else ts[:-1]
         
         # I. Compute SVDs
         Omega = np.vstack([self.X1, self.Ups])
         Ug,Sg,Vgt = svd(Omega, full_matrices=False)
         U,S,Vt = svd(self.X2, full_matrices=False)
+
+        threshold = kwargs.get('threshold')
         if np.any(threshold):
             # Allow for independent thresholding
             t1,t2 = 2*[threshold] if np.isscalar(threshold) else threshold
@@ -176,16 +204,21 @@ class DMDc:
         # II. Compute operators
         n,_ = self.X2.shape
         self.Atilde = dag(U)@self.X2@dag(Vgt)@np.diag(1/Sg)@dag(Ug[:n,:])@U
-        self.Btilde = dag(U)@self.X2@dag(Vgt)@np.diag(1/Sg)@dag(Ug[n:,:])
+        self.Btilde = dag(U)@self.X2@dag(Vgt)@np.diag(1/Sg)@dag(Ug[n:,:]) 
 
-        # III. DMD modes        
+        # III. DMD modes   
+        self.A = self.X2@dag(Vgt)@np.diag(1/Sg)@dag(Ug[:n,:])     
         self.eigs, W = eig(self.Atilde)
-        self.A = self.X2@dag(Vgt)@np.diag(1/Sg)@dag(Ug[:n,:])
         self.modes = self.A@U@W
         
-        # Also need Btilde -> B -> continuous B operator 
+        # IV. Control (TODO: Btilde -> B -> continuous B operator) 
         self.B = self.X2@dag(Vgt)@np.diag(1/Sg)@dag(Ug[n:,:])
-#         self.Bcurly = # TODO
+
+    @classmethod
+    def from_full(cls, X, Ups, ts, **kwargs):
+        X2 = X[:, 1:]
+        X1 = X[:, :-1]
+        return cls(X2, X1, Ups, ts, **kwargs)
 
     def predict(self, control=None, x0=None):
         '''
@@ -197,9 +230,8 @@ class DMDc:
         '''
         Ups = self.Ups if control is None else control
         xt = self.X1[:,0] if x0 is None else x0
-        res = [xt]
-        # Add initial point and ignore last point to match orig_timesteps
-        for ut in Ups.T[:-1]:
+        res = []
+        for ut in Ups.T:
             xt_1 = self.A@xt + self.B@ut
             xt = xt_1
             res.append(xt_1)
@@ -213,26 +245,46 @@ class DMDc:
 # -- DMDc with separate access to lhs and rhs.
 # ------------------------------------------------------
 class bilinear_DMDc:
-    def __init__(self, X2, X1, U, sample_times, shift, threshold=None):
+    def __init__(self, X2, X1, U, ts, **kwargs):
         '''
-        Parameters:
-            X2, X1: Offset data matrices with columns storing states at sequential times
-            U: The control signal(s) acting on X.
-            sample_times:
-            shift: Need to know about time delays to match times in the u.X control term
-        ''' 
-        self.shift = shift
+        X2 = A X1 + U B X1
 
+        Parameters:
+            X2, X1: 
+                Offset data matrices with columns storing states at sequential times
+            U: 
+                The bilinear control signal(s) acting on X1 in the RHS term U B X1.
+            ts:
+                Sequential time series at which the X (or X1) measurements were taken.
+
+        Optional Parameters:
+            shift: default 0 
+                Number of time delays (necessary to match times in the u B X control term)
+            threshold: default None
+
+        Updates:
+            self.X2,self.X1: data
+            self.U: bilinear control signal
+            self.Ups: control signal U.X1
+            self.t0: initial time
+            self.dt: timestep
+            self.orig_timesteps: list of times for X1
+            self.A (self.Atilde): discrete time dynamical operator (projected)
+            self.B (self.Btilde): discrete time control operator (projected)
+            self.eigs: eigenvalues of Atilde
+            self.modes: eigenvectors of A and Atilde
+        ''' 
         self.U = U
         self.X1 = X1
         self.X2 = X2
 
-        self.t0 = sample_times[0]
-        self.dt = sample_times[1] - sample_times[0]
-        self.orig_timesteps = sample_times
+        self.t0 = ts[0]
+        self.dt = ts[1] - ts[0]
+        self.orig_timesteps = ts if len(ts) == len(self.X1) else ts[:-1]
 
         # Partially unwrap delay embedding to make sure the correct control signals
         # are combined with the correct data times.
+        self.shift = kwargs.get('shift', 0)
         self.Ups = np.einsum('sit, sjt->sijt',
                              self.U.reshape(self.shift+1, -1, len(self.orig_timesteps)),
                              self.X1.reshape(self.shift+1, -1, len(self.orig_timesteps))
@@ -242,6 +294,8 @@ class bilinear_DMDc:
         Omega = np.vstack([self.X1, self.Ups])
         Ug,Sg,Vgt = svd(Omega, full_matrices=False)
         U,S,Vt = svd(self.X2, full_matrices=False)
+
+        threshold = kwargs.get('threshold')
         if np.any(threshold):
             # Allow for independent thresholding
             t1,t2 = 2*[threshold] if np.isscalar(threshold) else threshold
@@ -266,9 +320,8 @@ class bilinear_DMDc:
         self.A = self.X2@dag(Vgt)@np.diag(1/Sg)@dag(Ug[:n,:])
         self.modes = self.A@U@W
         
-        # Also need Btilde -> B -> continuous B operator 
+        # IV. Control (TODO: Btilde -> B -> continuous B operator) 
         self.B = self.X2@dag(Vgt)@np.diag(1/Sg)@dag(Ug[n:,:])
-#         self.Bcurly = # TODO
 
     def time_spectrum(self, t):
         '''
@@ -291,7 +344,7 @@ class bilinear_DMDc:
         the control signal from the initial fit.
 
         Parameters:
-            control: the time-delayed contro signal.
+            control: the time-delayed control signal.
         '''
         control = self.U if control is None else control
         xt = self.X1[:,0] if x0 is None else x0 # Flat array
